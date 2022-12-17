01c5cb9cc9113b1bb610698e6679093a042cc2d0
MikiEremiki <52360858+MikiEremiki@users.noreply.github.com>
2022-12-17 19:03:01 +0300

    1) Обновил import'ы 2) Добавил команду help 3) Добавил ConversationHandler для гибкой настройки report_of_balances

--- bot.py
+++ bot.py
@@ -1,10 +1,24 @@
 from datetime import time
 from pytz import timezone
 
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    CallbackQueryHandler,
+    ConversationHandler
+)
 
 from settings import API_TOKEN
-from handlers import notify_assignees_evening, notify_assignees_morning, report_of_balances, report_of_warehouse
+from handlers import (
+    notify_assignees_evening,
+    notify_assignees_morning,
+    report_of_balances,
+    report_of_warehouse,
+    configure_report_of_balances,
+    generate_list_of_names,
+    end_configure_report_of_balances,
+    help_command
+)
 from utilites import echo
 
 
@@ -14,8 +28,21 @@ def bot():
     jq = updater.job_queue
 
     dp.add_handler(CommandHandler('echo', echo))
+    dp.add_handler(CommandHandler('help', help_command))
     dp.add_handler(CommandHandler('report_of_balances', report_of_balances))
     dp.add_handler(CommandHandler('report_of_warehouse', report_of_warehouse))
+
+    configure_report_of_balances_handler = ConversationHandler(
+        entry_points=[CommandHandler('config_rep_of_bal', configure_report_of_balances)],
+        states={
+            1: [
+                CallbackQueryHandler(end_configure_report_of_balances, pattern='^Закрыть$'),
+                CallbackQueryHandler(generate_list_of_names),
+            ],
+        },
+        fallbacks=[CommandHandler('help', help_command)],
+    )
+    dp.add_handler(configure_report_of_balances_handler)
 
     jq.run_daily(notify_assignees_morning, time(9, 00, 00, tzinfo=timezone('Europe/Moscow')))
     jq.run_daily(notify_assignees_evening, time(18, 00, 00, tzinfo=timezone('Europe/Moscow')))
