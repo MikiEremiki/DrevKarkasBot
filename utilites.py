ff2da973c97f43fc1a8db3683ec1f503f1c3edfc
MikiEremiki <52360858+MikiEremiki@users.noreply.github.com>
2022-12-17 19:00:27 +0300

    Добавил функции для чтения и записи списка имен для отчета в файл txt

--- utilites.py
+++ utilites.py
@@ -22,3 +22,34 @@ def echo(update: Update, context: CallbackContext):
     text = f'chat.id: {update.effective_chat.id}'
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text=text)
+
+
+def write_list_of_names_in_file(list_of_names):
+    with open('list_chosen_name_for_report.txt', 'w', encoding='utf-8') as f:
+        for item in list_of_names:
+            f.write(item + '\n')
+
+
+def read_list_of_names_in_file():
+    with open('list_chosen_name_for_report.txt', mode='r', encoding='utf-8') as f:
+        list_name_for_report = []
+        for line in f:
+            list_name_for_report.append(line.replace('\n', ''))
+    return list_name_for_report
+
+
+def get_list_chosen_name_for_report():
+    try:
+        list_name_for_report = read_list_of_names_in_file()
+    except FileNotFoundError:
+        list_name_for_report = []
+        write_list_of_names_in_file(list_name_for_report)
+
+    return list_name_for_report
+
+
+def delete_message_for_job_in_callback(context: CallbackContext):
+    context.bot.delete_message(
+        chat_id=context.job.context['chat_id'],
+        message_id=context.job.context['message_id']
+    )
