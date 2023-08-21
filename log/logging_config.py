b758d1deb02746cd317452dda72a45bf10584150
MikiEremiki <mr.eremin.ma@ya.ru>
2023-08-21 11:27:43 +0300

    Add logging

---
+++ log/logging_config.py
@@ -0,0 +1,24 @@
+import logging.handlers
+
+LOG_FILENAME = 'log/log.txt'
+
+
+def load_log_config():
+    root = logging.getLogger()
+    bf = logging.Formatter('{asctime:16s}|{name:20s}|{levelname:8s}|{message}',
+                           datefmt='%y%m%d %H:%M:%S',
+                           style='{',
+                           )
+    handler = logging.handlers.RotatingFileHandler(LOG_FILENAME,
+                                                   mode='w',
+                                                   maxBytes=1024000,
+                                                   backupCount=5,
+                                                   encoding='utf-8',
+                                                   )
+    handler.setFormatter(bf)
+    root.addHandler(handler)
+
+    logger = logging.getLogger('bot')
+    logger.setLevel(logging.DEBUG)
+
+    return logger
